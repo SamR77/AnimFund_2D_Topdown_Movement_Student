3b9361372ec6450af73d96f73ab5736177f5b5f0
Sam R [home] <Snrobichaud@gmail.com>
2021-03-21 13:06:52 -0300

    Updated Project

--- Assets/Credits_References.cs
+++ Assets/Credits_References.cs
@@ -5,3 +5,4 @@ using UnityEngine;
 
 // Brackeys Tutorial https://www.youtube.com/watch?v=whzomFgjT50
 // Tiny RPG Forest Pack https://assetstore.unity.com/packages/2d/characters/tiny-rpg-forest-114685
+// Hundred Fires Games https://www.youtube.com/watch?v=VsSq_Ispo3Q
